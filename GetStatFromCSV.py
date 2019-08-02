32a845d1edfbdf35d3aa3fcc4e7dcb548e4ac661
Dmitry Alexandrov <dalexandrov@domain.corp>
2019-08-02 18:37:31 +0300

    Add multiple table and average value

--- GetStatFromCSV.py
+++ GetStatFromCSV.py
@@ -1,13 +1,19 @@
 #https://pandas.pydata.org/pandas-docs/stable/user_guide
 #https://khashtamov.com/ru/pandas-introduction/
-#1. get file
-#2. get dirty table from file
-#3. remove empty duplicates (where Elapsed is null)
-#4. 
 import pandas
 import os
+import sys
 
+""" Get the statistic from Testrail export files
+This script parse exported files from Testrail. For export
+sould be selected columns:
+- Case ID
+- Elapsed
+- Selenium Profile
+- Status
+- Type
 
+"""
 class GetTableFromFile:
     statTableDirty = None
     statTableClean = None
@@ -16,6 +22,11 @@ class GetTableFromFile:
     
     def __init__(self):
         self.sourceFile = self.__chooseFile()        
+        self._parseFile()
+        self._cleanTable()
+
+    def __init__(self, fileName):
+        self.sourceFile = fileName
         self._parseFile()
         self._cleanTable()
 
@@ -35,7 +46,7 @@ class GetTableFromFile:
             else:
                 stop = False
         return fileList[fileCode]
-
+    
     def _parseFile(self, fileName = None):
         if fileName is None:
             print("Using default file")
@@ -50,16 +61,16 @@ class GetTableFromFile:
         #prevent SettingWithCopyWarning is displayed
         #https://stackoverflow.com/questions/20625582/how-to-deal-with-settingwithcopywarning-in-pandas
         self.statTableClean = self.statTableDirty.copy()
-        print("Count of Case IDs before cleaning is \n{} \n"
-            "Remove entires, where Elapsed is NULL"
-              .format(self.statTableClean["Case ID"].count()))                
+        #print("Count of Case IDs before cleaning is \n{} \n"
+        #    "Remove entires, where Elapsed is NULL"
+        #      .format(self.statTableClean["Case ID"].count()))                
         self.statTableClean = self.statTableClean[
             self.statTableClean["Elapsed"].notnull()]
 
-        print("Count of Case IDs after cleaning is \n{} \n"
-            "Remove 's' char from Elapsed column, transform to Integer"
-            "and Remove duplicates for total stat"
-              .format(self.statTableClean["Case ID"].count()))
+        #print("Count of Case IDs after cleaning is \n{} \n"
+        #    "Remove 's' char from Elapsed column, transform to Integer"
+        #    "and Remove duplicates for total stat"
+        #      .format(self.statTableClean["Case ID"].count()))
         self.statTableClean["Elapsed"] = self.statTableClean["Elapsed"].apply(
                                             lambda x: x.replace("s", ""))
         self.statTableClean["Elapsed"] = pandas.to_numeric(
@@ -69,8 +80,8 @@ class GetTableFromFile:
         self.statTableUniqs.drop_duplicates(subset = "Case ID", 
                                             keep = "last", inplace = True)
 
-        print("Count of Case IDs after remove duplicates \n{}"
-              .format(self.statTableUniqs["Case ID"].count()))
+        #print("Count of Case IDs after remove duplicates \n{}"
+        #      .format(self.statTableUniqs["Case ID"].count()))
 
     def getCountByType(self, type = "total"):
         #print("Get count of {} tests".format(type.title()))
@@ -82,21 +93,33 @@ class GetTableFromFile:
                             
     def getCountOfStatuses(self, status):
         return self.statTableClean[self.statTableClean["Status"] == 
-                                            status.title()]["Case ID"].count()
+            status.title()]["Case ID"].count()
     
     def getElapsedByStatus(self, status = "total"):
         if status.lower() == "total":
-            return self.statTableClean["Elapsed"].sum() // 3600
-        return self.statTableClean[self.statTableClean["Status"] == 
-                                            status.title()]["Elapsed"].sum() // 3600
+            return self.statTableClean[self.statTableClean["Type"] == 
+            "Automated"]["Elapsed"].sum() // 3600
+        #Use () for removing 'boolean series key will be reindexed to match dataframe index' warning
+        return round(self.statTableClean[(self.statTableClean["Type"] == 
+            "Automated") & (self.statTableClean["Status"] == 
+            status.title())]["Elapsed"].sum() / 3600, 2)
+
+    def getAverage(self):
+        return round(self.statTableClean[self.statTableClean["Type"] == 
+            "Automated"]["Elapsed"].mean() / 60, 2)
 
 if __name__ == "__main__":
+    ACTION_MENU_START = ("Please choose action:\n"
+        "0: Exit\n"
+        "1: Add file for stat\n"
+        "2: Add all neighbours files\n")
     ACTION_MENU = ("Please choose action:\n"
         "0: Exit\n"
         "1: Add file for stat\n"
-        "2: Count uniq tests by types\n"
-        "3: Elapsed stat\n")
-    actionCode = input(ACTION_MENU)
+        "2: Add all neighbours files\n"
+        "3: Count uniq tests by types\n"
+        "4: Elapsed stat for Automated\n")
+    actionCode = input(ACTION_MENU_START)
     tables = dict()
     stop = True
     i = 0
@@ -107,8 +130,14 @@ if __name__ == "__main__":
                 result = GetTableFromFile()
                 tables[i] = result
                 i += 1
-            #get stats
             if actionCode is "2":
+                for each in os.listdir():
+                    if os.path.isfile("./{}".format(each)) and ".csv" in each:
+                        result = GetTableFromFile(each)
+                        tables[i] = result
+                        i += 1
+            #get stats
+            if actionCode is "3":
                 totalTable = pandas.DataFrame(index = [
                     #Type
                     "Automated",
@@ -138,14 +167,15 @@ if __name__ == "__main__":
                         totalTable["dif{}".format(key)] = \
                             totalTable.iloc[:,key] - totalTable.iloc[:,key-1]
                 print(totalTable)
-            if actionCode is "3":
+            if actionCode is "4":
                 elapsedTable = pandas.DataFrame(index = [                    
                     #Elapsed
-                    "Passed Elapsed",
-                    "Retest Elapsed",
-                    "Blocked Elapsed",
-                    "Failed Elapsed",
-                    "Total Elapsed"
+                    "Passed Elapsed (h)",
+                    "Retest Elapsed (h)",
+                    "Blocked Elapsed (h)",
+                    "Failed Elapsed (h)",
+                    "Total Elapsed (h)",
+                    "Mean value (m)"
                 ])
                 for key,value in tables.items(): 
                     elapsedTable[value.sourceFile] = [
@@ -154,7 +184,8 @@ if __name__ == "__main__":
                         value.getElapsedByStatus("Retest"),
                         value.getElapsedByStatus("Blocked"),
                         value.getElapsedByStatus("Failed"),
-                        value.getElapsedByStatus()
+                        value.getElapsedByStatus(),
+                        value.getAverage()
                     ]
                     if key > 0:
                         elapsedTable["dif{}".format(key)] = \
@@ -163,4 +194,4 @@ if __name__ == "__main__":
             actionCode = input(ACTION_MENU)
         else:
             #exit
-            stop = False
+            sys.exit()
